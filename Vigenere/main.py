76e119c65ec0b3751b83f6020c1ece41ceb4ce35
Quentin HOPP <quentin.hopp@outlook.com>
2020-09-14 13:50:54 +0200

    Comment Vigenere code
    + Test commit signature

--- Vigenere/main.py
+++ Vigenere/main.py
@@ -7,14 +7,21 @@ class VigenereCipher(object):
     
     def encode(self, text):
         res = []
+
+        # We convert every characters in its unicode code point
         text = list(map(ord,text))
+        
+        # We're iterating through the key (repeating itself if it's longer than `text`)
         for (i,k) in enumerate(cycle(self.key)):
-            if i == len(text): break
+            if i == len(text): 
+                break
             if chr(text[i]) not in self.alphabet: 
+                # We ignore every characters that do not appear in `self.alphabet`
                 res.append(chr(text[i]))
                 continue
             
-            value = ((text[i] - 97 + ord(k) - 97) % 26) + 97
+            # We sum the position of every text letter and corresponding key letter in the alphabet
+            value = ((text[i] - 97 + ord(k) - 97) % 26) + 97 
             res.append(chr(value))
         return "".join(res)
     
